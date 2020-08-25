2c9acf8473c57c933c032ff2b06c4078f3566faa
Josh Morrison <josh.morrison1999@gmail.com>
2020-08-24 22:35:56 -0400

    Initial Commit

---
+++ PitchDetection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PitchDetection
+//
+//  Created by Josh Morrison on 8/24/20.
+//  Copyright © 2020 Josh Morrison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
